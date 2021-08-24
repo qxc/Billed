144ce3934623ff8e9dde7dc7f5e28c2080889775
Kevin <cold.ferret@gmail.com>
2021-08-24 11:47:11 -0500

    WiP player

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody m_Rigidbody;
+    public float m_Thrust = 100f;
+    private bool can_Jump = true;
+    public BoxCollider feet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetButton("Jump") && can_Jump)
         {
-            this.transform.Translate(Vector3.up * Time.deltaTime);
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            m_Rigidbody.AddForce(transform.up * m_Thrust);
+            can_Jump = false;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
         {
-            this.transform.Translate(Vector3.down * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            this.transform.Translate(Vector3.left * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            this.transform.Translate(Vector3.right * Time.deltaTime);
+            can_Jump = true;
         }
     }
 }
