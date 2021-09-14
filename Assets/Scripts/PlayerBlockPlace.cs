eab4108b17410353c4c4eac95949546f291c788c
Kevin Riley <k.riley08@gmail.com>
2021-09-14 10:14:48 -0700

    Added block placement

--- Assets/Scripts/PlayerBlockPlace.cs
+++ Assets/Scripts/PlayerBlockPlace.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerBlockPlace : MonoBehaviour
 {
-    private bool is_previewing_blocks;
+    private bool is_previewing_block;
+    private bool is_placing_block;
     private GameObject block_preview;
     public GameObject block_preview_prefab;
+    public GameObject block_prefab;
     private Movement movement_script;
-    private float block_placement_distance = 2f;
+    private float block_placement_distance = 3f;
     
     // Start is called before the first frame update
     void Start()
@@ -19,37 +21,40 @@ public class PlayerBlockPlace : MonoBehaviour
         movement_script = gameObject.GetComponent<Movement>();
     }
 
-    /**
-     int round(double i, int v){
-    return Math.round(i/v) * v;
-     * 
-    */
     Vector3 RoundPosition(Transform trans)
     {
         Vector3 collider_size = block_preview_prefab.transform.localScale;
-        Debug.Log(collider_size);
-        
-        return new Vector3(Mathf.RoundToInt(trans.position.x), Mathf.RoundToInt(trans.position.y), Mathf.RoundToInt(trans.position.z));
+        float x_len = collider_size.x;
+        return new Vector3(Mathf.RoundToInt(trans.position.x / x_len) * x_len, trans.position.y, trans.position.z);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (is_previewing_blocks)
+        if (is_previewing_block)
         {
+            Vector3 spawn_loc;
+            Vector3 grid_position = RoundPosition(transform);
+            
             if (movement_script.is_facing_left)
-            {
-                Vector3 grid_position = RoundPosition(transform);
-                Vector3 spawn_loc = new Vector3(grid_position.x + -block_placement_distance, grid_position.y, grid_position.z);
-                block_preview.transform.position = spawn_loc;
+            {   
+                spawn_loc = new Vector3(grid_position.x + -block_placement_distance, grid_position.y, grid_position.z);
             } 
             else
             {
-                Vector3 grid_position = RoundPosition(transform);
-                Vector3 spawn_loc = new Vector3(grid_position.x + block_placement_distance, grid_position.y, grid_position.z);
-                block_preview.transform.position = spawn_loc;
+                spawn_loc = new Vector3(grid_position.x + block_placement_distance, grid_position.y, grid_position.z);
             }
+
+            block_preview.transform.position = spawn_loc;
             block_preview.SetActive(true);
+            if (is_placing_block)
+            {
+                GameObject block = Instantiate(block_prefab) as GameObject;
+                block.transform.position = block_preview.transform.position;
+                is_placing_block = false;
+                is_previewing_block = false;
+                block_preview.SetActive(false);
+            }
         }
     }
     
@@ -57,14 +62,15 @@ public class PlayerBlockPlace : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire3"))
         {
-            is_previewing_blocks = true;
+            is_previewing_block = true;
             Debug.Log(transform.position);
         }
         if (Input.GetButtonUp("Fire3"))
         {
-            Debug.Log("Not previewing blocks");
-            block_preview.SetActive(false);
-            is_previewing_blocks = false;
+            if (is_previewing_block)
+            {
+                is_placing_block = true;
+            }
         }
     }
 }
