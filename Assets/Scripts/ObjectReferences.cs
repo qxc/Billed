0129a80a02328dc27d203edd07fd7bd7aabbf6ec
Raphael Serota <raphael.serota@gmail.com>
2021-10-11 16:54:59 -0500

    enable monster spawner in combat phase

--- Assets/Scripts/ObjectReferences.cs
+++ Assets/Scripts/ObjectReferences.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectReferences : MonoBehaviour
-{
+public class ObjectReferences : MonoBehaviour {
     public GameObject Player1;
     public GameObject Player2;
     public GameObject Boss;
     public GameObject MonsterSpawner;
+    [HideInInspector]
+    public int currentPhase; // 0/1/2 == building/combat/levelup
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        currentPhase = 0;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Debug.Log(currentPhase);
+        if (Input.GetButtonDown("Select") && currentPhase == 0) {
+            Debug.Log("NEXT PHASE!");
+            currentPhase = 1;
+            MonsterSpawner.SetActive(true);
+        }
     }
 }
