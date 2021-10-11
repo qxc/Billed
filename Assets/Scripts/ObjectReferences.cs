783e262c12e43359038ef67d9f52d95be64517bd
Raphael Serota <raphael.serota@gmail.com>
2021-10-11 17:00:51 -0500

    should rename objectreferences

--- Assets/Scripts/ObjectReferences.cs
+++ Assets/Scripts/ObjectReferences.cs
@@ -7,11 +7,15 @@ public class ObjectReferences : MonoBehaviour {
     public GameObject Player2;
     public GameObject Boss;
     public GameObject MonsterSpawner;
+    public float combatPhaseLength;
     [HideInInspector]
     public int currentPhase; // 0/1/2 == building/combat/levelup
+    private float phaseStartTime;
     // Start is called before the first frame update
     void Start() {
         currentPhase = 0;
+        combatPhaseLength = 12;
+        phaseStartTime = Time.time;
     }
 
     // Update is called once per frame
@@ -20,7 +24,13 @@ public class ObjectReferences : MonoBehaviour {
         if (Input.GetButtonDown("Select") && currentPhase == 0) {
             Debug.Log("NEXT PHASE!");
             currentPhase = 1;
+            phaseStartTime = Time.time;
             MonsterSpawner.SetActive(true);
+        }
+        if ( currentPhase == 1 && phaseStartTime + combatPhaseLength < Time.time ) {
+            Debug.Log("combat phase has ended");
+            MonsterSpawner.SetActive(false);
+
         }
     }
 }
